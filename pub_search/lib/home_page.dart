f33d2faa8664b2c1a4475f1c25325f718a062c24
Edgar Froes <edgar.froes@gmail.com>
2023-03-06 17:00:12 -0300

    (fix) home screen scrolling

--- pub_search/lib/home_page.dart
+++ pub_search/lib/home_page.dart
@@ -19,8 +19,8 @@ class _HomePageState extends State<HomePage> {
   void initState() {
     super.initState();
 
-    _textFieldFocusNode.addListener(_updateVisualization);
-    _textFieldController.addListener(_updateVisualization);
+    _textFieldFocusNode.addListener(_updatePackageListRender);
+    _textFieldController.addListener(_updatePackageListRender);
 
     _updateRecentSearchesFromStorage();
   }
@@ -36,31 +36,30 @@ class _HomePageState extends State<HomePage> {
 
   bool get _isInSearchingMode => _textFieldController.text.length >= 2;
 
-  bool _showNoRecentSearchesMessage = true;
+  bool _showEmptyRecentSearches = true;
   bool _showRecentSearches = false;
   bool _showNoPackagesFoundMessage = false;
   bool _showSearchResult = false;
   bool _errorFetchingPackages = false;
   bool _loadingPackages = false;
 
-  void _updateVisualization() {
+  void _updatePackageListRender() {
     setState(() {
       _showRecentSearches = _recentSearches.isNotEmpty && !_isInSearchingMode;
 
-      _showNoRecentSearchesMessage =
-          _recentSearches.isEmpty && !_isInSearchingMode;
+      _showEmptyRecentSearches = _recentSearches.isEmpty && !_isInSearchingMode;
     });
   }
 
   void _updateRecentSearchesFromStorage() {
     final storedRecentSearches =
-        (_box.read<List<dynamic>>('packages') ?? []).cast<String>();
+        (_box.read<List<dynamic>>(_storageBoxName) ?? []).cast<String>();
 
     if (storedRecentSearches.isNotEmpty) {
       _recentSearches.clear();
       _recentSearches.addAll(storedRecentSearches);
 
-      _updateVisualization();
+      _updatePackageListRender();
     }
   }
 
@@ -69,7 +68,7 @@ class _HomePageState extends State<HomePage> {
       setState(() {
         _loadingPackages = true;
         _showNoPackagesFoundMessage = false;
-        _showNoRecentSearchesMessage = false;
+        _showEmptyRecentSearches = false;
         _showRecentSearches = false;
         _errorFetchingPackages = false;
       });
@@ -106,19 +105,19 @@ class _HomePageState extends State<HomePage> {
       _showSearchResult = false;
       _errorFetchingPackages = false;
       _showNoPackagesFoundMessage = false;
-      _showNoRecentSearchesMessage = _recentSearches.isEmpty;
+      _showEmptyRecentSearches = _recentSearches.isEmpty;
       _showRecentSearches = _recentSearches.isNotEmpty;
       _foundPackages.clear();
     });
   }
 
-  Future<void> _goToPackageDetailsPage(String packageName) {
+  Future _goToPackageDetailsPage(String packageName) async {
     _textFieldFocusNode.unfocus();
     _textFieldController.clear();
 
-    return Navigator.of(context).push(
+    await Navigator.of(context).push(
       MaterialPageRoute(
-        builder: (context) => PackageDetailsPage(
+        builder: (_) => PackageDetailsPage(
           packageName: packageName,
         ),
       ),
@@ -131,78 +130,102 @@ class _HomePageState extends State<HomePage> {
       body: SafeArea(
         child: Container(
           width: double.infinity,
+          padding: const EdgeInsets.symmetric(horizontal: 10),
           alignment: Alignment.center,
-          child: SizedBox(
-            width: 237,
-            child: Column(
-              children: [
-                const SizedBox(
-                  height: 100,
+          child: Container(
+            constraints: const BoxConstraints(maxWidth: 237),
+            child: CustomScrollView(
+              slivers: [
+                SliverList(
+                  delegate: SliverChildListDelegate(
+                    [
+                      const SizedBox(
+                        height: 100,
+                      ),
+                      Center(
+                        child: SearchTextField(
+                          controller: _textFieldController,
+                          focusNode: _textFieldFocusNode,
+                          onChanged: _searchPackages,
+                        ),
+                      ),
+                      const SizedBox(
+                        height: 6,
+                      ),
+                      if (_loadingPackages && !_showSearchResult)
+                        Container(
+                          padding: const EdgeInsets.only(top: 30),
+                          alignment: Alignment.center,
+                          child: const CircularProgressIndicator(),
+                        ),
+                      if (_showEmptyRecentSearches)
+                        const RoundedContainer(
+                          child: Text(
+                            'No Recent Searches',
+                            textAlign: TextAlign.center,
+                          ),
+                        ),
+                      if (_errorFetchingPackages)
+                        const RoundedContainer(
+                          child: Text(
+                            'Error searching packages',
+                            textAlign: TextAlign.center,
+                          ),
+                        ),
+                      if (_showRecentSearches)
+                        PackageList(
+                          title: 'Recent Searches',
+                          packages: _recentSearches,
+                          onSelectPackage: _goToPackageDetailsPage,
+                        ),
+                      if (_showNoPackagesFoundMessage)
+                        const RoundedContainer(
+                          child: Text('No packages found'),
+                        ),
+                      if (_showSearchResult && _isInSearchingMode)
+                        PackageList(
+                          packages: _foundPackages,
+                          onSelectPackage: (String packageName) async {
+                            _addPackageToRecentSearches(packageName);
+
+                            await _goToPackageDetailsPage(packageName);
+                          },
+                        ),
+                    ],
+                  ),
                 ),
-                SearchTextField(
-                  controller: _textFieldController,
-                  focusNode: _textFieldFocusNode,
-                  onChanged: _searchPackages,
-                ),
-                const SizedBox(
-                  height: 6,
-                ),
-                if (_loadingPackages && !_showSearchResult)
-                  const CircularProgressIndicator(),
-                if (_showNoRecentSearchesMessage)
-                  const RoundedContainer(
-                    child: Text(
-                      'No Recent Searches',
-                      textAlign: TextAlign.center,
-                    ),
-                  ),
-                if (_errorFetchingPackages)
-                  const RoundedContainer(
-                    child: Text(
-                      'Error searching packages',
-                      textAlign: TextAlign.center,
-                    ),
-                  ),
-                if (_showRecentSearches)
-                  PackageList(
-                    title: 'Recent Searches',
-                    packages: _recentSearches,
-                    onSelectPackage: _goToPackageDetailsPage,
-                  ),
-                if (_showNoPackagesFoundMessage)
-                  const RoundedContainer(
-                    child: Text('No packages found'),
-                  ),
-                if (_showSearchResult)
-                  PackageList(
-                    packages: _foundPackages,
-                    onSelectPackage: (String packageName) async {
-                      _savePackageOnRecentSearches(packageName);
+                SliverFillRemaining(
+                  hasScrollBody: false,
+                  fillOverscroll: true,
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.end,
+                    children: [
+                      Padding(
+                        padding: const EdgeInsets.only(top: 30),
+                        child: TextButton(
+                          onPressed: () async {
+                            _textFieldController.clear();
+                            _textFieldFocusNode.unfocus();
 
-                      await _goToPackageDetailsPage(packageName);
-                    },
-                  ),
-                const Spacer(),
-                TextButton(
-                  onPressed: () async {
-                    _textFieldController.clear();
-                    _textFieldFocusNode.unfocus();
+                            setState(() {
+                              _foundPackages.clear();
+                              _recentSearches.clear();
+                            });
 
-                    setState(() {
-                      _foundPackages.clear();
-                      _recentSearches.clear();
-                    });
+                            await _box.erase();
 
-                    await _box.erase();
-
-                    _updateVisualization();
-                  },
-                  child: Row(
-                    mainAxisSize: MainAxisSize.min,
-                    children: const [
-                      Icon(Icons.clear),
-                      Text(
-                        'Clear all data',
+                            _updatePackageListRender();
+                          },
+                          child: Row(
+                            mainAxisSize: MainAxisSize.min,
+                            children: const [
+                              Icon(Icons.clear),
+                              Text(
+                                'Clear all data',
+                              ),
+                            ],
+                          ),
+                        ),
                       ),
                     ],
                   ),
@@ -215,13 +238,15 @@ class _HomePageState extends State<HomePage> {
     );
   }
 
-  Future<void> _savePackageOnRecentSearches(String packageName) async {
+  Future<void> _addPackageToRecentSearches(String packageName) async {
     if (!_recentSearches.contains(packageName)) {
       setState(() {
         _recentSearches.insert(0, packageName);
       });
 
-      await _box.write('packages', _recentSearches);
+      await _box.write(_storageBoxName, _recentSearches);
     }
   }
 }
+
+const String _storageBoxName = 'packages';
