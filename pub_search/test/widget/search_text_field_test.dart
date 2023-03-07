4314f1c4f0a40ede1579f168be1b44320fba3e58
Edgar Froes <edgar.froes@gmail.com>
2023-03-07 01:54:33 -0300

    (chore) add onChanged test to search text field

--- pub_search/test/widget/search_text_field_test.dart
+++ pub_search/test/widget/search_text_field_test.dart
@@ -1,9 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:mockito/annotations.dart';
+import 'package:mockito/mockito.dart';
 import 'package:pub_search/theme.dart';
 import 'package:pub_search/widgets/search_text_field.dart';
 import 'package:rive/rive.dart';
+
+@GenerateNiceMocks([MockSpec<OnChanged>()])
+import 'search_text_field_test.mocks.dart';
 
 Widget makeTestable(Widget widget) => MaterialApp(
       theme: mainTheme,
@@ -21,14 +26,14 @@ void main() {
       final focusNode = FocusNode();
       final textEditingController = TextEditingController();
 
+      final mockOnChanged = MockOnChanged();
+
       await tester.pumpWidget(
         makeTestable(
           SearchTextField(
             focusNode: focusNode,
             controller: textEditingController,
-            onChanged: (value) {
-              //
-            },
+            onChanged: mockOnChanged.onChanged,
           ),
         ),
       );
@@ -43,9 +48,9 @@ void main() {
       expect(animationFinder, findsOneWidget);
       expect(textFieldFinder, findsOneWidget);
       expect(
-          (tester.firstWidget(animatedOpacityFinder) as AnimatedOpacity)
-              .opacity,
-          equals(1));
+        (tester.firstWidget(animatedOpacityFinder) as AnimatedOpacity).opacity,
+        equals(1),
+      );
       expect(animatedOpacityFinder, findsOneWidget);
 
       await tester.tap(textFieldFinder);
@@ -53,27 +58,39 @@ void main() {
       await tester.pumpAndSettle();
 
       expect(
-          (tester.firstWidget(animatedOpacityFinder) as AnimatedOpacity)
-              .opacity,
-          equals(0));
+        (tester.firstWidget(animatedOpacityFinder) as AnimatedOpacity).opacity,
+        equals(0),
+      );
 
-      await tester.enterText(textFieldFinder, text);
+      for (var i = 1; i <= text.length; i++) {
+        final typedText = text.substring(0, i);
+
+        await tester.enterText(textFieldFinder, typedText);
+      }
+
+      // await tester.enterText(textFieldFinder, text);
 
       await tester.pumpAndSettle();
 
+      for (var i = 1; i <= text.length; i++) {
+        final typedText = text.substring(0, i);
+
+        verify(mockOnChanged.onChanged(typedText)).called(1);
+      }
+
       expect(
-          (tester.firstWidget(animatedOpacityFinder) as AnimatedOpacity)
-              .opacity,
-          equals(0));
+        (tester.firstWidget(animatedOpacityFinder) as AnimatedOpacity).opacity,
+        equals(0),
+      );
 
       focusNode.unfocus();
 
       await tester.pumpAndSettle();
 
       expect(
-          (tester.firstWidget(animatedOpacityFinder) as AnimatedOpacity)
-              .opacity,
-          equals(0));
+        (tester.firstWidget(animatedOpacityFinder) as AnimatedOpacity).opacity,
+        equals(0),
+      );
 
       await tester.tap(textFieldFinder);
       await tester.pumpAndSettle();
@@ -88,9 +105,15 @@ void main() {
       await tester.pumpAndSettle();
 
       expect(
-          (tester.firstWidget(animatedOpacityFinder) as AnimatedOpacity)
-              .opacity,
-          equals(1));
+        (tester.firstWidget(animatedOpacityFinder) as AnimatedOpacity).opacity,
+        equals(1),
+      );
     },
   );
+}
+
+class OnChanged {
+  onChanged(String value) => (_) {
+        //
+      };
 }
