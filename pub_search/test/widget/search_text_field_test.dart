0e49ddf89ecb112e07df288f0f4dd8107931f743
Edgar Froes <edgar.froes@gmail.com>
2023-03-07 01:36:58 -0300

    (chore) add search text field test

---
+++ pub_search/test/widget/search_text_field_test.dart
@@ -0,0 +1,96 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:pub_search/theme.dart';
+import 'package:pub_search/widgets/search_text_field.dart';
+import 'package:rive/rive.dart';
+
+Widget makeTestable(Widget widget) => MaterialApp(
+      theme: mainTheme,
+      home: Material(
+        child: Scaffold(
+          body: widget,
+        ),
+      ),
+    );
+
+void main() {
+  testWidgets(
+    'Search icon animation should appear or disappear upon user interaction',
+    (WidgetTester tester) async {
+      final focusNode = FocusNode();
+      final textEditingController = TextEditingController();
+
+      await tester.pumpWidget(
+        makeTestable(
+          SearchTextField(
+            focusNode: focusNode,
+            controller: textEditingController,
+            onChanged: (value) {
+              //
+            },
+          ),
+        ),
+      );
+
+      await tester.pumpAndSettle();
+
+      final animationFinder = find.byType(RiveAnimation).hitTestable();
+      final textFieldFinder = find.byType(TextField);
+      final animatedOpacityFinder = find.byType(AnimatedOpacity);
+      const text = 'abc';
+
+      expect(animationFinder, findsOneWidget);
+      expect(textFieldFinder, findsOneWidget);
+      expect(
+          (tester.firstWidget(animatedOpacityFinder) as AnimatedOpacity)
+              .opacity,
+          equals(1));
+      expect(animatedOpacityFinder, findsOneWidget);
+
+      await tester.tap(textFieldFinder);
+
+      await tester.pumpAndSettle();
+
+      expect(
+          (tester.firstWidget(animatedOpacityFinder) as AnimatedOpacity)
+              .opacity,
+          equals(0));
+
+      await tester.enterText(textFieldFinder, text);
+
+      await tester.pumpAndSettle();
+
+      expect(
+          (tester.firstWidget(animatedOpacityFinder) as AnimatedOpacity)
+              .opacity,
+          equals(0));
+
+      focusNode.unfocus();
+
+      await tester.pumpAndSettle();
+
+      expect(
+          (tester.firstWidget(animatedOpacityFinder) as AnimatedOpacity)
+              .opacity,
+          equals(0));
+
+      await tester.tap(textFieldFinder);
+      await tester.pumpAndSettle();
+
+      for (var i = 0; i < text.length; i++) {
+        await tester.sendKeyEvent(LogicalKeyboardKey.backspace);
+        await tester.pumpAndSettle();
+      }
+
+      focusNode.unfocus();
+
+      await tester.pumpAndSettle();
+
+      expect(
+          (tester.firstWidget(animatedOpacityFinder) as AnimatedOpacity)
+              .opacity,
+          equals(1));
+    },
+  );
+}
